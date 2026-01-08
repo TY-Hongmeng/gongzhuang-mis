828703a344956707c2d04e197b43c8b240838edc
TY-Hongmeng <815602375@qq.com>
2026-01-08 16:26:41 +0800

    fix: use HashRouter for GitHub Pages to avoid blank route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
 import Login from "./pages/Login";
