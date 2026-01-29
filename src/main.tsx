60d47e82d32a7c51af1d45d62c9d2b68debe7872
Administrator <admin@example.com>
2026-01-30 07:37:26 +0800

    Force unregister service worker on production to avoid stale bundle

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,16 @@ import './index.css'
 
 installApiInterceptor()
 console.log('App Version: v1.5.9')
+if ('serviceWorker' in navigator) {
+  try {
+    navigator.serviceWorker.getRegistrations?.().then(regs => {
+      regs.forEach(r => r.unregister())
+    }).catch(() => {})
+    caches.keys().then(names => {
+      Promise.all(names.map(n => caches.delete(n)))
+    }).catch(() => {})
+  } catch {}
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
