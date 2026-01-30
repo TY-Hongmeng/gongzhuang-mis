63e49c0fecfcccd3f40f101d543f4ed01b1c6f97
Administrator <admin@example.com>
2026-01-30 19:55:46 +0800

    v1.7.8: Simplify purchase order creation to direct Supabase client insert/update; remove Functions forwarding; bump version and log

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { installApiInterceptor } from './utils/api'
 import './index.css'
 
 installApiInterceptor()
-console.log('App Version: v1.7.7')
+console.log('App Version: v1.7.8')
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
