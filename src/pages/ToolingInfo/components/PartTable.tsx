e43dd2004ea9c08b0925697c3d088a1396194213
Administrator <admin@example.com>
2026-03-26 15:51:36 +0800

    chore: auto sync v1.20.32 2026-03-26 15:51:36

--- src/pages/ToolingInfo/components/PartTable.tsx
+++ src/pages/ToolingInfo/components/PartTable.tsx
@@ -233,7 +233,7 @@ export const PartTable: React.FC<PartTableProps> = memo(({
         </Space>
       </div>
       
-      <div style={{ border: '2px solid blue' }}>
+      <div style={{ border: '2px solid blue', marginTop: 8 }}>
         <Table
           ref={tableRef}
           rowKey="id"
